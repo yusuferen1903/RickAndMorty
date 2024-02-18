bd8b64cbd5b264ca6187ccfcb011eeef3759b124
yusuferen.uneyler <yusuferen.uneyler@dijitalkurye.com.tr>
2024-02-18 15:12:23 +0300

    foot header ve mainlayou sayfaları oluşturuldu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+const routes: Routes = [
+    //MainLayout Header ve Footerın Bulunduğu Ana Component içine ise children componentler gelerek sayfayı oluşturuyor.
+    {
+      path: '',
+      component: MainLayoutComponent,
+      children: [
+        {
+          path: '',
+          component: MainpageComponent
+        }
+  
+      ]
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
